79e67ca248413f3de210b8a972c65ac667b0e394
Kla35 <37518897+Kla35@users.noreply.github.com>
2019-02-28 16:19:28 +0100

    Ah

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const ytdl = require('ytdl-core');
 const LeagueJS = require('./node_modules/leaguejs/lib/LeagueJS.js');
 const PREFIX = "!";
 var moment = require('moment');
+process.env.LEAGUE_API_PLATFORM_ID = 'euw1'
 
-const leagueJs = new LeagueJS(process.env.LEAGUE_API_KEY,{PLATFORM_ID: euw1}, {
+const leagueJs = new LeagueJS(process.env.LEAGUE_API_KEY, {
         useV4: true, // enables apiVersion overrides
         // these values override default values in Config.js
         // values omitted will use defaults from Config.js!
@@ -59,6 +60,7 @@ bot.on('message', message => {
  
  bot.on('message', message => {
     if (message.content === '!test') {
+	
 	leagueJs.Summoner
     .gettingByName('Klanat')
     .then(data => {
